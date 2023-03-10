5a97880b6329e5292af6f95b5ad8a4917b4e847f
kingpreyansh <preyanshdutta@gmail.com>
2023-03-10 09:28:03 -0500

    Changes

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require("./routes/index.route");
 
-const cors = require("cors")
-const dotenv = require('dotenv')
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
-app.use(cors())
-app.use(bodyParser.urlencoded( { extended: false }));
-app.use(bodyParser.json({limit: '50mb', type: 'application/json'}));  
-
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 
 app.use("/", routes);
-app.use(express.static(path.join(__dirname + '/public')));
-app.use(express.json())
-app.listen(process.env.port, () => {
-    console.log('server is running : ' + process.env.port);
-})
+app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("server is running : " + 3000);
+});
