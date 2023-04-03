5045803f0606590455df67a2260f298233b1478b
danildoduh <129189764+danildoduh@users.noreply.github.com>
2023-04-03 11:54:20 -0400

    fixed order of result

--- backend/removebg.py
+++ backend/removebg.py
@@ -1,13 +1,16 @@
 import os
-import sys
 import cv2
+import sys
 from PIL import Image
 from transparent_background import Remover
+
 
 currentDir = os.path.dirname(__file__)
 remover = Remover(fast=True, jit=True)
 
+
 def removeBg(imagePath):
+
 	unique_filename = sys.argv[2]
 	extension = imagePath.rsplit('.', 1)[1].lower()
 	try:
@@ -17,15 +20,14 @@ def removeBg(imagePath):
 		masks_dir = os.path.join(currentDir, 'public/masks/')
 		img = Image.open(imagePath).convert('RGB')
 		img.save(inputs_dir+unique_filename+'.'+extension)
-		out = remover.process(img, type='map') # mask
-		Image.fromarray(out).save(masks_dir + unique_filename + '.png')
 		out = remover.process(img) # remove background
 		Image.fromarray(out).save(results_dir + unique_filename + '.png')
+		out = remover.process(img, type='map') # mask
+		Image.fromarray(out).save(masks_dir + unique_filename + '.png')
 
-		return "---Success---"
+		return "---Success---"	
 	except:
 		return "---Empty image---"
-
 
 imgPath = sys.argv[1]
 print(removeBg(imgPath))
