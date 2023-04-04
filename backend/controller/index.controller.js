c6130b094c7497fdd70acb7a5f00d33ad02e3754
danildoduh <129189764+danildoduh@users.noreply.github.com>
2023-04-04 12:56:04 -0400

    fixed removedb requesting method

--- backend/controller/index.controller.js
+++ backend/controller/index.controller.js
@@ -1,16 +1,25 @@
 const { exec } = require("child_process");
+const request = require('request');
+
 const removeBg = async (req, res) => {
   try {
     const fileName = req.file.filename.replace(/\.[^/.]+$/, "");
-    const cmd = `python removebg.py ${req.file.path} ${fileName}`;
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.log("error occurs at cmd_img : " + error);
-        res.status(400).json({ message: "Something happened wrong" });
-      }
-      res.status(200).json({ image: `http://localhost:3000/results/${fileName}.png` , mask : `http://localhost:3000/masks/${fileName}.png` });
+    request('http://127.0.0.1:5000/removeBackground?imagePath=' + req.file.path + '&unique_filename='+fileName, function (error, response, body) {
+        if (error) {
+          console.log("error occurs: " + error);
+          res.status(400).json({ message: "Something happened wrong" });
+        }
+        res.status(200).json({ image: `http://localhost:3000/results/${fileName}.png` , mask : `http://localhost:3000/masks/${fileName}.png` });
+      }); 
+    // const cmd = `python removebg.py ${req.file.path} ${fileName}`;
+    // exec(cmd, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.log("error occurs at cmd_img : " + error);
+    //     res.status(400).json({ message: "Something happened wrong" });
+    //   }
+    //   res.status(200).json({ image: `http://localhost:3000/results/${fileName}.png` , mask : `http://localhost:3000/masks/${fileName}.png` });
       
-    });
+    // });
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: "Something happened wrong" });
